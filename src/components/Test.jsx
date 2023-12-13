6660d66f3c8da672ae5f0f2f2a54e9be5fa15d33
youngminkk <mmmin0610@gmail.com>
2023-12-13 18:19:41 +0900

    create prj

---
+++ src/components/Test.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+<div>
+    <h1>안녕하세요</h1>
+</div>
---
+++ src/views/Homeview.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Test from "../components/Test";
+
+
+const HomeView = () => {
+  return (
+      <>
+      <div>
+        <Test />
+      </div>
+      </>
+  )
+}
+
+export default HomeView
