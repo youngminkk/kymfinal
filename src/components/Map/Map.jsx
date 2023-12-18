80ce035df7b2cd0197a07854cbedd7e697faa218
youngminkk <mmmin0610@gmail.com>
2023-12-18 18:20:09 +0900

    scheduler update

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Map = () => {
-  useEffect(() => {
-    const mapDiv = document.getElementById('map_div');
+  // map 인스턴스를 저장할 ref를 생성합니다.
+  const mapRef = useRef(null);
 
-    if (mapDiv && !mapDiv.firstChild) {
-      new window.Tmapv2.Map("map_div", {
+  useEffect(() => {
+    // 지도를 담을 DOM 요소를 가져옵니다.
+    const mapDiv = document.getElementById('map_div');
+ 
+    // mapRef.current가 null이면 새 Tmapv2.Map 인스턴스를 생성합니다.
+    if (!mapRef.current && mapDiv) {
+      mapRef.current = new window.Tmapv2.Map("map_div", {
         center: new window.Tmapv2.LatLng(37.566481622437934,126.98502302169841),
-        width: "890px",
-        height: "400px",
+        width: "100vh",
+        height: "40vh",
         zoom: 15
       });
     }
   }, []);
 
-  return <div id='map_div' className='map__container'/>;
+  return(
+    <div id='map_div' className='map__container'></div>
+  ) 
 };
 
 export default Map;
--- src/components/Scheduler/Scheduler.jsx
+++ src/components/Scheduler/Scheduler.jsx
@@ -1,13 +1,57 @@
-import React from "react";
-import {text} from "../../constants";
-
-
+import React, { useState } from 'react';
+import { Scheduler as MyScheduler } from "@aldabil/react-scheduler";
+import { ko } from 'date-fns/locale';
 
 const Scheduler = () => {
-    return(
-    <div>
-        <h1 className="scheduler">{text[2].text}</h1>
+  const [events, setEvents] = useState([
+    {
+      id: 1,
+      title: "Meeting",
+      start: new Date(),
+      end: new Date(new Date().setHours(new Date().getHours() + 1)),
+      resourceId: 1,
+      admin_id: 300,
+    }
+  ]);
+
+  const { handleEventsChange, handleCellClick, handleEventClick } = UseSchedulerEvents(events, setEvents);
+
+  return (
+    <div id='scheduler'>
+      <div className='scheduler__header'>안녕하세요</div>
+      <div className='scheduler__container'>
+        <MyScheduler
+          view="day"  
+          locale={ko}
+          week={{
+            weekDays: [0, 1, 2, 3, 4, 5, 6],
+            weekStartOn: 1,
+            startHour: 9,
+            endHour: 17,
+            step: 60,
+          }}
+          month={{
+            weekDays: [6, 0, 1, 2, 3, 4, 5],
+            weekStartOn: 1,
+            startHour: 9,
+            endHour: 17,
+            step: 60,
+          }}
+          events={events}
+          onEventDrop={(event) => handleEventsChange({ event, action: 'update' })}
+          onEventResize={(event) => handleEventsChange({ event, action: 'update' })}
+          onEventEdit={(event) => handleEventClick(event.id)} // 수정은 클릭 이벤트로 처리
+          onEventDelete={(event) => handleEventsChange({ event, action: 'delete' })}
+          onCellClick={handleCellClick}
+          onEventView={(eventId) => handleEventClick(eventId)} // onEventView를 handleEventClick으로 대체
+          viewerExtraComponent={(fields, event) => (
+            <ViewerExtra fields={fields} event={event} />
+          )}
+          className="scheduler__calendar"
+        />
+      </div>
     </div>
-    )
-}
-export default Scheduler
+  );
+};
+
+export default Scheduler;
